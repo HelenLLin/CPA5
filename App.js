9822f04013f2a351043d1e6f6c694749dc71ab62
Helen Lin <63370363+helenlin746@users.noreply.github.com>
2021-12-14 03:40:32 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
-import React, {createContext} from 'react';
+import React, {useState, useEffect, createContext} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import {Text, View, StyleSheet, TextInput, Button, TouchableOpacity, Image  } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import Main from './CPA5/Main.js'
 import List from './CPA5/List.js'
@@ -12,13 +13,13 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return(
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Main' component={Main} />
-        <Stack.Screen name='Lists' component={List} />
-        <Stack.Screen name='About' component={About} />
-        <Stack.Screen name='History' component={History} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+            <Stack.Navigator initialRouteName='Home'>
+                <Stack.Screen name='Main' component={Main} />
+                <Stack.Screen name='Lists' component={List} />
+                <Stack.Screen name='About' component={About} />
+                <Stack.Screen name='History' component={History} />
+            </Stack.Navigator>
+        </NavigationContainer>
   );
 }
