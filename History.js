8583f91a06a46fafc73288d5a30a5e139998e584
Helen Lin <63370363+helenlin746@users.noreply.github.com>
2021-12-13 22:21:59 -0500

    Add files via upload

---
+++ History.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function History({Navigation}) {
+
+}
+
+export default History;
