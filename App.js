074201ade8d4a1a4ab5833ef535f8f99d0d1e1bb
Helen Lin <63370363+helenlin746@users.noreply.github.com>
2021-12-13 22:25:24 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,24 @@
+import React, {createContext} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {Text, View, StyleSheet, TextInput, Button, TouchableOpacity, Image  } from 'react-native';
+
+import Main from './CPA5/Main.js'
+import List from './CPA5/List.js'
+import About from './CPA5/About.js'
+import History from './CPA5/History.js'
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Main' component={Main} />
+        <Stack.Screen name='Lists' component={List} />
+        <Stack.Screen name='About' component={About} />
+        <Stack.Screen name='History' component={History} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
