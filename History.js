8f43f05b716b21a1c972c55005a7c893aac39666
Helen Lin <63370363+helenlin746@users.noreply.github.com>
2021-12-13 22:24:09 -0500

    Delete History.js

--- History.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-function History({Navigation}) {
-
-}
-
-export default History;
