e89a01e9ec63b71b7f92bb62e858f4a4164f61e4
Helen Lin <63370363+helenlin746@users.noreply.github.com>
2021-12-14 03:41:55 -0500

    Update About.js

--- components/About.js
+++ components/About.js
@@ -3,23 +3,30 @@ import { View, Text, TextInput, Button, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const image={uri:'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'};
-
 function About() {
   return(
     <View style={{flex: 1, flexDirection: 'column', alignItems: 'center', justifyContent: 'center', backgroundColor: '#ECC9C7', }}>
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', }}>
+        <Text style = {{fontSize: 40, fontFamily: 'Playfair Display', fontWeight:'bold', textAlign: 'center', }}>
+            About
+        </Text>
         <Text style = {{fontSize: 20, fontFamily: 'Playfair Display', textAlign: 'center', }}>
-          This app helps you organize the tasks you have to complete.
-          {"\n"}
-          You can enter tasks that you need to complete as well as the date you have to complete it by.
-          {"\n"}
-          The item will be added to a list.
-          {"\n"}
-          You can clear the list at any point.
-          {"\n"}
-          {"\n"}
-          In-Progress Implementations: deletion of single items, history page, ...
+            This app helps you organize the tasks you have to complete.
+            {"\n"}
+            You can enter tasks that you need to complete as well as the date you have to complete it by.
+            {"\n"}
+            The item will be added to a list.
+            {"\n"}
+            You can clear the list at any point.
+            {"\n"}
+            {"\n"}
+            In Development:
+            {"\n"}
+            • Deletion of single items from list
+            {"\n"}
+            • Dedicated log-in page that stores your to-do list with a login
+            {"\n"}
+            • Expanding to-do list elements for further details
         </Text>
       </View>
     </View>
